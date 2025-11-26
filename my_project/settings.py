37d212872f666497e0db2ee83e735f570202ab42
jolantadjatlova <jolanta.djatlova@icloud.com>
2025-11-26 21:06:31 +0000

    Update Django settings (ALLOWED_HOSTS / DEBUG)

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4j8r_-r&!7*5t&ih2u(^$4&5umqm+m$==y9%-awumcnlpzkmp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
